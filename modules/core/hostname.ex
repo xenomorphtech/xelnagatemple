11c2de8f8e0c5711e487ec999bff2bd42f1bb12a
vans163 <vans_163@yahoo.com>
2020-10-02 13:59:21 -0400

    delete old modules/

--- modules/core/hostname.ex
+++
@@ -1,8 +0,0 @@
-defmodule XNT.Hostname do
-    def set(state) do
-        host = state.host
-        cmd = "hostnamectl set-hostname #{host.hostname}"
-        {"", 0} = XNT.SSHWrap.execute(state, cmd)
-        :ok
-    end
-end
