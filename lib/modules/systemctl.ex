b1d4c6205efe3883c3414d304c2643070756c795
vans163 <me@vans163.com>
2025-07-13 13:19:40 -0400

    systemd

--- lib/modules/systemctl.ex
+++ lib/modules/systemctl.ex
@@ -28,4 +28,16 @@ defmodule XNT.Module.Systemctl do
     {_, 0} = XNT.SSHWrap.execute(state, cmd)
     :ok
   end
+
+  def is_active(state, name) do
+    cmd = "systemctl is-active #{name}"
+    {_, 0} = XNT.SSHWrap.execute(state, cmd)
+    :ok
+  end
+
+  def is_enabled(state, name) do
+    cmd = "systemctl is-enabled #{name}"
+    {_, 0} = XNT.SSHWrap.execute(state, cmd)
+    :ok
+  end
 end
